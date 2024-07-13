96a6ba668a749fdbe768640a1d97138af8ca7b75
Gaurav <gauravahuja2012@gmail.com>
2024-07-13 20:55:52 +0530

    Added controller, view and route file

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -1,2 +1,18 @@
 class CalculatorsController < ApplicationController
+  def index
+    redirect_to new_calculator_path
+  end
+  
+  def new
+  end
+
+  def create
+    input = params[:input]
+    begin
+      @result = StringCalculator.add(input)
+    rescue => e
+      @error = e.message
+    end
+    render :new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :calculators, only: [:index, :new, :create]
+  root 'calculators#new'
 end
