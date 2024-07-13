d06aaea0d80b560b88b897577ca62115f05efbf5
Gaurav <gauravahuja2012@gmail.com>
2024-07-13 18:30:07 +0530

    Added test case 1 defination

--- test/controllers/calculators_controller_test.rb
+++ test/controllers/calculators_controller_test.rb
@@ -2,6 +2,9 @@ require "test_helper"
 
 class CalculatorsControllerTest < ActionDispatch::IntegrationTest
   test 'calculates sum with default delimiters' do
+    post calculators_path, params: { input: "1,2,3" }
+    assert_response :success
+    assert_includes @response.body, "6"  # Adjust based on your response format
   end
 
   test 'calculates sum with custom delimiter' do
