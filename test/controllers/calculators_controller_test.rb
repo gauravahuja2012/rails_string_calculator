6478153faa0ad89b45a77832f96eae1ed28f2dc4
Gaurav <gauravahuja2012@gmail.com>
2024-07-13 18:27:24 +0530

    Created blank test cases

--- test/controllers/calculators_controller_test.rb
+++ test/controllers/calculators_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class CalculatorsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'calculates sum with default delimiters' do
+  end
+
+  test 'calculates sum with custom delimiter' do
+  end
+
+  test 'calculates sum with newlines' do
+  end
+
+  test 'returns error for negative numbers' do
+  end
+
+  test 'returns 0 for empty input' do
+  end
 end
